24621aa84a9898488505940b5ac116b3328205ad
SoumikDebnath1 <132799614+SoumikDebnath1@users.noreply.github.com>
2024-01-10 01:59:08 +0530

    go

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -131,16 +131,16 @@
                     <ul className="sidebar" >
                       
                         <li onClick={closeSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-                        <li><a href="#" style={{ textDecoration: 'none', color: 'rgb(253, 253, 253)' }}>Menu</a></li>
+                        <li><a href="" style={{ textDecoration: 'none', color: 'rgb(253, 253, 253)' }}>Menu</a></li>
                         <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Home</a></li>
                         <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Predict</a></li>
                         <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>About</a></li>
                     </ul>
-                    <ul className='hideOnMobile1'>
+                    <ul >
                         <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Shop</a></li>
-                        <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Home</a></li>
-                        <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Predict</a></li>
-                        <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>About</a></li>
+                        <li className='hideOnMobile1'><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Home</a></li>
+                        <li className='hideOnMobile1'><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>Predict</a></li>
+                        <li className='hideOnMobile1'><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>About</a></li>
                         <li className="menu-button" onClick={showSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
                     </ul>
                 </nav>
