e78fe7f4b9409be152f2ec45eb7ee56f86872467
SoumikDebnath1 <132799614+SoumikDebnath1@users.noreply.github.com>
2024-01-10 12:19:41 +0530

    go

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -131,8 +131,9 @@
                     <ul className="sidebar" >
                       
                         <li onClick={closeSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-                        <li><a href="" style={{ textDecoration: 'none', color: 'rgb(253, 253, 253)' }}>Menu</a></li>
+                        <li><a style={{ textDecoration: 'none', color: 'rgb(253, 253, 253)' }}>Menu</a></li>
                         
+                        <li><Link to='/cart' onClick={() => handleMenuClick("shop")}>{getTotalCartItems()}<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></Link></li>
                         <li><Link to='/' onClick={() => handleMenuClick("shop")}>Home</Link></li>
                         <li><Link to='/Womens' onClick={() => handleMenuClick("shop")}>Womens</Link></li>
                         <li><Link to='/mens' onClick={() => handleMenuClick("shop")}>Mens</Link></li>
@@ -140,13 +141,13 @@
                         <li><a href="" style={{ color: 'rgb(246, 156, 156)', textDecoration: 'none' }}>About</a></li>
                     </ul>
                     <ul >
-                    <li></li>
+                    <li><a >MY STORE</a></li>
                         
-                        <li className='hideOnMobile1'><Link to='/' onClick={() => handleMenuClick("shop")} className='a1'>SHOP</Link></li>
                         <li className='hideOnMobile1'><Link to='/' onClick={() => handleMenuClick("shop")} className='a1'>SHOP</Link></li>
                         <li className='hideOnMobile1'><Link to='/Womens' onClick={() => handleMenuClick("shop")}>Womens</Link></li>
                         <li className='hideOnMobile1'><Link to='/mens' onClick={() => handleMenuClick("shop")}>Mens</Link></li>
                         <li className='hideOnMobile1'><Link to='/kids' onClick={() => handleMenuClick("shop")}>Kids</Link></li>
+                        <li><Link to='/cart' onClick={() => handleMenuClick("shop")}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>{getTotalCartItems()}</Link></li>
                         <li className="menu-button" onClick={showSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
                     </ul>
                 </nav>
